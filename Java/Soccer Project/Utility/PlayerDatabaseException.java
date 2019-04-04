05881f6dab2ea62a147df9d2d7b849f3cb27fdb7
SamoraJourdan <47771894+SamoraJourdan@users.noreply.github.com>
2019-04-04 11:39:06 +0200

    Add files via upload

---
+++ Java/Soccer Project/Utility/PlayerDatabaseException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author pc
+ */
+public class PlayerDatabaseException extends Exception {
+   public PlayerDatabaseException() {}
+   public PlayerDatabaseException(String message) {
+   super(message);
+  }
+}
